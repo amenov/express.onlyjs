afc652e0ba1979960e63f4180855183c52e92eb3
Amenov <amenov.abdulsalam@gmail.com>
2021-07-13 21:57:09 +0600

    2157

--- middleware.js
+++ middleware.js
@@ -1,40 +1,7 @@
-const flat = require('flat')
+const only = require('amenov.only')
 
 module.exports = () => (req, res, next) => {
-  req.only = (...keys) => {
-    if (Array.isArray(keys[0])) keys = keys[0]
-
-    const flatBody = flat.flatten(req.body)
-    const flatKeys = []
-
-    function flattenKeys(keys, prevKey = '') {
-      for (const key of keys) {
-        if (typeof key === 'string') {
-          flatKeys.push(prevKey ? `${prevKey}.${key}` : key)
-        } else {
-          for (const [_key, _keys] of Object.entries(key)) {
-            flattenKeys(_keys, prevKey ? `${prevKey}.${_key}` : _key)
-          }
-        }
-      }
-    }
-
-    flattenKeys(keys)
-
-    const siftedFlatBody = {}
-
-    for (const flatKey of flatKeys) {
-      const value = flatBody[flatKey]
-
-      if (value !== undefined) {
-        siftedFlatBody[flatKey] = value
-      }
-    }
-
-    const siftedBody = flat.unflatten(siftedFlatBody)
-
-    return Object.keys(siftedBody).length ? siftedBody : null
-  }
+  req.only = (...keys) => only(req.body, ...keys)
 
   next()
 }
